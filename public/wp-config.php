a5a6d4a9ac0da1073fc69d25117df63e00e228b4
Kim Ellefsen <kim@ellefsen.me>
2017-08-17 09:07:47 +0200

    Disable loading of Dotenv. Requires refactoring and removal of old local-config. Dotenv also fails on Deploy to button

--- public/wp-config.php
+++ public/wp-config.php
@@ -15,7 +15,7 @@
  */
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-(new \Dotenv\Dotenv(__DIR__.'/../'))->load();
+// (new \Dotenv\Dotenv(__DIR__.'/../'))->load();
 
 if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 
@@ -104,7 +104,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  *
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
- */	
+ */
 $table_prefix  = "wp_";
 
 /**
